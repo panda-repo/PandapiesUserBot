5cbd056627e3f0ef362dba7e9dc75af9559cbcaf
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-05-01 19:51:03 +0700

    Create write.py

---
+++ PandapiesUserBot/plugins/write.py
@@ -0,0 +1,20 @@
+from pyrogram import filters
+
+from config import HANDLER, OWNER_ID
+from PandapiesUserBot import Pandapies
+
+
+@Pandapies.on_message(filters.command("write", prefixes=HANDLER) & filters.user(OWNER_ID))
+async def write(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("ᴘʟᴢ ɢɪᴠᴇ ᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴡʀɪᴛᴇ ғɪʀsᴛ ")
+    m = await message.reply_text("ᴡʀɪᴛɪɴɢ...")
+    name = (
+        message.text.split(None, 1)[1]
+        if len(message.command) < 3
+        else message.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    hand = "https://apis.xditya.me/write?text=" + name
+    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ...")
+    await m.delete()
+    await message.reply_photo(hand, caption="**Meet Me Here🙈 @Besties_XD ✨🥀**")
