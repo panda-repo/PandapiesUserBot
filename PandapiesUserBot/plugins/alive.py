a3b62e6b112c7397323c20082e41d0c11fd6124d
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-05-01 18:54:44 +0700

    Membuat alive.py

---
+++ PandapiesUserBot/plugins/alive.py
@@ -0,0 +1,19 @@
+import time
+
+from pyrogram import __version__ as pyrover
+from pyrogram import filters
+
+from config import ALIVE_TEXT, HANDLER, NEKO, OWNER_ID
+from PandapiesUserBot import StartTime, get_readable_time, neko
+
+
+@neko.on_message(filters.command("alive", prefixes=HANDLER) & filters.user(OWNER_ID))
+def alive(_, m):
+    you = neko.get_me()
+    start_time = time.time()
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    uptime = get_readable_time((time.time() - StartTime))
+    m.reply_photo(
+        PANDAPIES, caption=ALIVE_TEXT.format(you.mention, pyrover, ping_time, uptime)
+    )
