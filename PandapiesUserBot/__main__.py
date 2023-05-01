9ff75ed30de125f800afc8e7f1238484a29d0dfa
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-05-01 19:56:19 +0700

    Delete __main__.py

--- PandapiesUserBot/__main__.py
+++
@@ -1,8 +0,0 @@
-from config import LOG_GROUP_ID
-from NekoUserBot import bot, neko
-
-if __name__ == "__main__":
-    neko.start()
-    bot.run()
-    with bot:
-        bot.send_message(f"{LOG_GROUP_ID}", "Nyaa Neko Ready Boi!")
