0057b1855498c07383ed26844b8eecf624cb355e
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-05-01 20:43:28 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+LOG_GROUP_ID = -1001834104418
+OWNER_ID = [5523216254]
+HANDLER = ["~", ".", "!"]
+ALIVE_TEXT = """ **~  Pandapies System:**
+━━━━━━━━━━━━━━━━━━━
+
+❥ **𝑂𝑊𝑁𝐸𝑅**: {}
+❥ **𝑉𝐸𝑅𝑆𝐼𝑂𝑁:** {}
+❥ **𝑃𝑂𝑁𝐺 𝑀𝑠:** {}
+❥ **𝑈𝑃𝑇𝐼𝑀𝐸:** {}
+
+━━━━━━━━━━━━━━━━━━━
+**Meet Me Here🙈 @Pandapies ✨🥀**
+"""
+BOT_TOKEN = "5801133323:AAGE3wxMd0pHUavzezCxYt2fbQ7cp7KhlYU"
+API_ID = "21298319"
+API_HASH = "98430ee9bb9d17b261fd64d6a5d5c0b4"
+SESSION = "BQAhIHQAcRq4uwrB9nKKevwjkjJjzb09DjEiZkPICp4Q77JEe2tmaL_lSVYrul2NktBMzKhlbbdhoLjtWZhd9aSEIPf5gsu4JDnld2t-wqosjlO8_eRqZrmnMn-JxuK0R9roAYatqlrFEDZ6ztahTRZChMrFhLEGdFN0No4WS-wd5lRzPka3JRxBBB3ArzkISASJbTCCzGh16GjR_WvPYFXVZZ-tF8ebMisEGom4fXlXGu6t3WgFRxHcQbkG9qPQmU_BxYCb7VW40aVq5hkQaBq2iCBwlTGeBtHUmHc2VPO8ELVqzEgU59vxl4KuwEGV6qOCSOFC7q8eay6eZMPFgrlUzwewAAAAFJNZd-AA"
+
+Pandapies = "https://telegra.ph/file/bf18fb39dad5fb3ab9e7e.png"
