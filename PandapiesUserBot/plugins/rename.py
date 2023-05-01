ba6992cf5402be43254b195a13ae74477ca92368
Panda Repo <128521829+panda-repo@users.noreply.github.com>
2023-05-01 19:42:52 +0700

    Create rename.py

---
+++ PandapiesUserBot/plugins/rename.py
@@ -0,0 +1,23 @@
+ import os
+
+from pyrogram import filters
+
+from config import HANDLER, OWNER_ID
+from PandapiesUserBot import Pandapies
+
+
+@Pandapies.on_message(filters.command("rename", prefixes=HANDLER) & filters.user(OWNER_ID))
+def rename(_, message):
+
+    try:
+        filename = message.text.replace(message.text.split(" ")[0], "")
+
+    except Exception as e:
+        print(e)
+
+    if reply := message.reply_to_message:
+        x = message.reply_text("Downloading.....")
+        path = reply.download(file_name=filename)
+        x.edit("Uploading.....")
+        message.reply_document(path)
+        os.remove(path)
